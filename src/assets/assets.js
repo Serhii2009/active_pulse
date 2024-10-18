80a883449c51a5327340e22162eea6194aa6b004
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-10-18 16:43:20 +0300

    add sign up form for mobile

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,12 @@ import logo_desctop from './logo_desctop.svg'
 import logo_mobile from './logo_mobile.svg'
 import side_menu from './side_menu.svg'
 import menu_bar_close from './menu_bar_close.svg'
+import sign_form_close from './sign_form_close.svg'
 
 export const assets = {
   logo_desctop,
   logo_mobile,
   side_menu,
   menu_bar_close,
+  sign_form_close,
 }
--- src/components/Sign/Sign.jsx
+++ src/components/Sign/Sign.jsx
@@ -1,7 +1,46 @@
 import './Sign.css'
+import { assets } from '../../assets/assets'
 
 const Sign = () => {
-  return <div>Sign</div>
+  return (
+    <div className="sign">
+      <div className="sign-container">
+        <img src={assets.sign_form_close} alt="" className="sign_form_close" />
+        <p className="sign-container-title">Sign up</p>
+        <p className="sign-container-description">
+          Signing up with a registered phone number or social account will log
+          you into your Traveler account
+        </p>
+
+        <div className="sign-container-chose-list">
+          <div className="sign-container-chose-email">Email address</div>
+          <div className="sign-container-chose-phone">Phone number</div>
+        </div>
+
+        <div className="sign-container-form">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="sign-container-form-email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="sign-container-form-password"
+          />
+
+          <div className="sign-container-form-button">Sign up</div>
+        </div>
+        <p className="sign-container-policy">
+          By creating an account, you accept our <span>Terms of Service</span>{' '}
+          and <span>Privacy Policy.</span>
+        </p>
+        <p className="sign-container-login">
+          Already have an account? <a href="">Log in</a>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Sign
