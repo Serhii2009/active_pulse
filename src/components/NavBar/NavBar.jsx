a7090bd51e0434fec4cc6b8f792ff0cfb4cf39ab
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-10-18 13:44:15 +0300

    add a side menu for mobile and tablete display

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,28 @@
 import './NavBar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const NavBar = () => {
+  const [modal, setModal] = useState(false)
+  const toggleModal = () => {
+    setModal(!modal)
+  }
   return (
     <div className="nav-bar">
       <div className="nav-bar-box">
-        <img
-          src={assets.logo_desctop}
-          alt=""
-          className="nav-bar-logo-desctop"
-        />
-        <img src={assets.logo_mobile} alt="" className="nav-bar-logo-mobile" />
+        <Link to="/">
+          <img
+            src={assets.logo_desctop}
+            alt=""
+            className="nav-bar-logo-desctop"
+          />
+          <img
+            src={assets.logo_mobile}
+            alt=""
+            className="nav-bar-logo-mobile"
+          />
+        </Link>
 
         <ul className="nav-bar-list">
           <li className="nav-bar-list-element">Training programs</li>
@@ -20,11 +32,36 @@ const NavBar = () => {
 
         <div className="nav-bar_cta">
           <div className="nav-bar-buttons">
-            <div className="nav-bar-sign-up">Sign Up</div>
+            <Link to="/signup">
+              <div className="nav-bar-sign-up">Sign Up</div>
+            </Link>
             <div className="nav-bar-log-in">Log In</div>
           </div>
-          <img src={assets.side_menu} alt="" className="nav-bar-side-menu" />
+          <img
+            onClick={toggleModal}
+            src={assets.side_menu}
+            alt=""
+            className="nav-bar-side-menu"
+          />
         </div>
+
+        {modal && (
+          <div className="nav-bar-modal">
+            <div className="nav-bar-modal-component">
+              <div className="nav-bar-modal-list">
+                <li>Training programs</li>
+                <li>News and articles</li>
+                <li>FAQ</li>
+              </div>
+              <img
+                onClick={toggleModal}
+                className="nav-bar-modal-close"
+                src={assets.menu_bar_close}
+                alt=""
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
